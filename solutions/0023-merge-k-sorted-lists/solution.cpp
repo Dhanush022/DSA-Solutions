4924b74dc595926775b51668749d595e8b8e1d28
Dhanush C K <164226113+Dhanush022@users.noreply.github.com>
2026-07-21 18:07:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (14.56%)

---
+++ solutions/0023-merge-k-sorted-lists/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    priority_queue<int, vector<int>, greater<int>> minheap;
+        for(ListNode* list : lists){
+            while(list!=nullptr){
+                minheap.push(list->val);
+                list=list->next;
+            }
+        }
+        ListNode dummy(0);
+        ListNode* merge=&dummy;
+        while(!minheap.empty()){
+            merge->next=new ListNode(minheap.top());
+            minheap.pop();
+            merge=merge->next;
+        }
+        return dummy.next;
+
+    }
+};
