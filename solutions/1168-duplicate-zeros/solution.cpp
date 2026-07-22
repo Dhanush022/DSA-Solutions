5f8eb31fcf59c1ec20c4c359917a6827aa265965
Dhanush C K <164226113+Dhanush022@users.noreply.github.com>
2026-07-22 08:39:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.9 MB (75.32%)

---
+++ solutions/1168-duplicate-zeros/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int count=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                count++;
+            }
+        }
+        int i=n-1;
+        int j=n+count-1;
+        while(i>=0){
+            if(j<n){
+                arr[j]=arr[i];
+            }
+            if(arr[i]==0){
+                j--;
+                if(j<n)arr[j]=0;
+            }
+            i--;
+            j--;
+        }
+    }
+};
