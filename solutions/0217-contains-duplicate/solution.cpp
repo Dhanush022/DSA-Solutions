141ad53727bdc7b9c1a9a1c845bd4003e8646981
Dhanush C K <164226113+Dhanush022@users.noreply.github.com>
2026-07-29 19:43:26 +0000

    Sync LeetCode submission Runtime - 75 ms (29.29%), Memory - 111.4 MB (26.05%)

--- solutions/0217-contains-duplicate/solution.cpp
+++ solutions/0217-contains-duplicate/solution.cpp
@@ -3,12 +3,13 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> seen;
         for(int num:nums){
-            if(seen.count(num)){
+            if(seen.contains(num)){
                 return true;
             }
             seen.insert(num);
-        
         }
         return false;
     }
 };
+//keep inserting the value in hashset and check if it already exixts.
+//TC:O(n) and SC:O(n)
