ca3bf9d19b4d1c04a8f726892324409f6d71536e
Dhanush C K <164226113+Dhanush022@users.noreply.github.com>
2026-07-29 19:59:19 +0000

    Sync LeetCode submission Runtime - 3 ms (66.26%), Memory - 14.8 MB (40.64%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> nummap;
-        int n = nums.size();
-        
-        for(int i=0;i<n;i++){
-            nummap[nums[i]]=i;
-        }
-        for(int i=0;i<n;i++){
-            int compli=target-nums[i];
-            if(nummap.count(compli) && nummap[compli]!=i){
-                return {i,nummap[compli]};
+        unordered_map<int, int> nummap;
+        for (int i = 0; i < nums.size(); i++) {
+            int compli = target - nums[i];
+            if (nummap.find(compli) != nummap.end()) {
+                return {nummap[compli], i}; 
             }
+            nummap[nums[i]] = i;
         }
+        
         return {};
     }
 };
+// One-pass Hash Map: Look backward for the complement.
+// As you loop, calculate complement (target - nums[i]).
+// If complement is in map, return {map[complement], i}.
+// Else, add current {nums[i]: i} to map for future lookups.
+// TC: O(n) and SC: O(n)
