84948813040d894c5dfee93b128619e4757fdd35
Dhanush C K <164226113+Dhanush022@users.noreply.github.com>
2026-08-05 19:41:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (36.12%)

---
+++ solutions/0371-sum-of-two-integers/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+      unsigned int ua=a,ub=b;
+      while(ub!=0){
+        unsigned int carry=(ua&ub)<<1;
+        ua=ua^ub;
+        ub=carry;
+      } 
+      return ua; 
+    }
+};
